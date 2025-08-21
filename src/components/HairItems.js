73cf9cbc96c033c7af27b224451a589585cb9649
yh <gugithub@gmail,com>
2025-08-21 21:29:51 +0900

    HairItems 이미지 경로 수정

--- src/components/HairItems.js
+++ src/components/HairItems.js
@@ -2,17 +2,18 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 function HairItems({ currentPage, itemsPerPage, updateTotalPages }) {
-  const [items, setItems] = useState([]);
+  const [originalData, setOriginalData] = useState([]);
+  const [visibleData, setVisibleData] = useState([]);
 
   useEffect(() => {
     axios
       .get("https://kku-git.github.io/nff_product/hair.json")
       .then((response) => {
-        const fetchedItems = response.data;
-        setItems(fetchedItems);
-
+        const sorted = [...response.data].sort((a, b) => a.id - b.id); // 오름차순 정렬
+        setOriginalData(sorted); // 원본 저장
+        setVisibleData(sorted);
         // 총 페이지 수 계산 및 업데이트
-        const totalPages = Math.ceil(fetchedItems.length / itemsPerPage);
+        const totalPages = Math.ceil(sorted.length / itemsPerPage);
         updateTotalPages(totalPages); // 부모 컴포넌트에 페이지 수 업데이트
       })
       .catch(() => {
@@ -20,13 +21,13 @@ function HairItems({ currentPage, itemsPerPage, updateTotalPages }) {
       });
   }, [itemsPerPage]);
 
-  if (items.length === 0) {
+  if (visibleData.length === 0) {
     return <p>LOADING...</p>;
   }
 
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
-  const currentItems = items.slice(startIndex, endIndex);
+  const currentItems = visibleData.slice(startIndex, endIndex);
 
   return (
     <div className="item-container">
