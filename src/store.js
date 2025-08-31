a8c2cbb5ec6da4571e38915f0887109e2796ad43
kku <gugithub@gmail.com>
2025-09-01 01:41:57 +0900

    store.js 수정

--- src/store.js
+++ src/store.js
@@ -18,16 +18,28 @@ let cart = createSlice({
 
     // 상품 삭제
     removeItem: function (state, action) {
-      return 
+      return state.filter((item) => item.id !== action.payload);
     },
 
     // 단순한 수량 증가 + 버튼
-    addCount: function (state, action) {},
+    addCount: function (state, action) {
+      let 번호 = state.findIndex((item) => item.id === action.payload);
+      state[번호].count++;
+    },
 
     // 단순한 수량 감소 - 버튼
-    decreaseCount: function (state, action) {},
+    decreaseCount: function (state, action) {
+      let 번호 = state.findIndex((item) => item.id === action.payload);
+
+      if (state[번호].count > 1) {
+        state[번호].count--;
+      }
+    },
 
     // 장바구니 비우기
+    clearCart: function (state, action) {
+      return [];
+    },
   },
 });
 
@@ -37,9 +49,14 @@ let wishlist = createSlice({
   reducers: {
     // 상품 추가 - 위시리스트 목록에 없으면 상품 추가 / 이미 있으면 무시 - 하트버튼
     // 상품 삭제
-    // 상품 장바구니로 이동
+    removeItem: function (state, action) {
+      return state.filter((item) => item.id !== action.payload);
+    },
     // 위시리스트 비우기
-    // 위시리스트에 있는 목록들 장바구니로 이동
+    clearWishlist: function (state, action) {
+      return [];
+    },
+    // 위시리스트에 있는 상품목록들 장바구니(cart)로 이동
   },
 });
 export default configureStore({
